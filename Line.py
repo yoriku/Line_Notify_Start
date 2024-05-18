2e25c18732f918a57baebfb3f5cdd80f869ad283
rikuya <69740420+yoriku@users.noreply.github.com>
2024-05-18 11:55:18 +0900

    Add files via upload

--- Line.py
+++ Line.py
@@ -1,7 +1,7 @@
 import requests
 
 def main():
-    send_line("Image Send", "test.jpg")
+    send_line("Image Send", "test.jpg", api_file="config/line.txt")
 
 def import_key(path="config/line.txt"):
     f = open(path, 'r', encoding='UTF-8')
@@ -9,8 +9,14 @@ def import_key(path="config/line.txt"):
     f.close()
     return key
 
-def send_line(message, file_path=None):
-    line_notify_token = import_key()
+def send_line(message, file_path=None, api_key=None, api_file=None):
+    if api_key is None and api_file is None:
+        raise ValueError("Need api_key or api_key_file. \n Prepare api key.")
+    if api_file is not None:
+        line_notify_token = import_key(file_path=api_file)
+    if api_key is not None:
+        line_notify_token = api_key
+
     line_notify_api = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': f'Bearer {line_notify_token}'}
     data = {'message': message}
